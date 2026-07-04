137dc7d46891e65796107ba19361d3480e21901c
Sphierson <Dev@aol.com>
2026-07-04 09:08:36 +0530

    changes

--- form.js
+++ form.js
@@ -1,9 +1,11 @@
-const signInBtnLink = document.querySelector('.signInBtn-link');
-const signUpBtnLink = document.querySelector('.signUpBtn-link');
 const wrapper = document.querySelector('.wrapper');
-signUpBtnLink.addEventListener('click', () => {
-    wrapper.classList.toggle('active');
-});
-signInBtnLink.addEventListener('click', () => {
-    wrapper.classList.toggle('active');
-});
+const registerLink = document.querySelector('.register-link');
+const loginLink = document.querySelector('.login-link');
+
+registerLink.onclick = () => {
+    wrapper.classList.add('active');
+}
+
+loginLink.onclick = () => {
+    wrapper.classList.remove('active');
+}
